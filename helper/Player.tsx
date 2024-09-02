75672af0f5e7d5b39d6b98a3429100c012cf7538
Nguyen Hoang Ky <nhktmdzhg@gmail.com>
2024-09-02 21:31:02 +0700

    room & player
    chac la gan xong, con vai problem

--- helper/Player.tsx
+++ helper/Player.tsx
@@ -1,15 +1,27 @@
 import {Card, sortHand} from './game_logic';
+import isValidPlay from './game_logic';
+
 export default class Player {
     private email: string;
     private name: string;
     private hand: Card[];
     private money: number;
+    private onTurn: boolean;
+    private preparedCards: Card[];
 
     constructor(email: string, name: string, money: number) {
         this.email = email;
         this.name = name;
         this.hand = [];
         this.money = money;
+        this.onTurn = true;
+        this.preparedCards = [];
+    }
+
+    public endGame(): void {
+        this.hand = [];
+        this.onTurn = true;
+        this.preparedCards = [];
     }
 
     public addCard(card: Card): void {
@@ -18,5 +30,36 @@ export default class Player {
 
     public sortHand(): void {
         this.hand = sortHand(this.hand);
+    }
+
+    public playCard(): Card[] {
+        this.hand = this.hand.filter(c => !this.preparedCards.includes(c));
+        const playedCards = this.preparedCards;
+        this.preparedCards = [];
+        return playedCards;
+    }
+
+    public getOnTurn(): boolean {
+        return this.onTurn;
+    }
+
+    public setOnTurn(onTurn: boolean): void {
+        this.onTurn = onTurn;
+    }
+
+    public addToPreparedCards(card: Card): void {
+        this.preparedCards.push(card);
+    }
+
+    public removeFromPreparedCards(card: Card): void {
+        this.preparedCards = this.preparedCards.filter(c => c !== card);
+    }
+
+    public isValidPlay(onBoardCards: Card[]): boolean {
+        return isValidPlay(this.preparedCards, onBoardCards);
+    }
+
+    public isEndGame(): boolean {
+        return this.hand.length === 0;
     }
 }
--- helper/Room.tsx
+++ helper/Room.tsx
@@ -1,8 +1,7 @@
 import {Card, sortHand} from './game_logic';
-import isValidPlay from './game_logic';
 import Player from './Player';
 
-class Room {
+export default class Room {
     private id: string;
     private name: string;
     private status: string;
@@ -10,6 +9,7 @@ class Room {
     private deck: Card[];
     private onBoardCards: Card[];
     private currentPlayer: number;
+    private prevWinner: number;
 
     constructor(name: string, createPlayer: Player) {
         this.id = Math.floor(Math.random() * 1000000).toString().padStart(6, '0');
@@ -19,6 +19,7 @@ class Room {
         this.deck = [];
         this.onBoardCards = [];
         this.currentPlayer = 0;
+        this.prevWinner = -1;
     }
 
     public addPlayer(player: Player): void {
@@ -29,18 +30,18 @@ class Room {
         this.players = this.players.filter(p => p !== player);
     }
 
-    public generateDeck(){
+    private generateDeck() {
         
     }
 
-    public shuffleDeck(): void {
+    private shuffleDeck(): void {
         for (let i = this.deck.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
         }
     }
 
-    public dealCards(): void {
+    private dealCards(): void {
         let curr = 0;
         while (this.deck.length > 0) {
             const currCard = this.deck.pop();
@@ -60,6 +61,44 @@ class Room {
         this.dealCards();
         for (const player of this.players) {
             player.sortHand();
+            player.setOnTurn(true);
         }
+    }
+
+    public isValidPlay(): boolean {
+        if (this.onBoardCards// khong phai 2, doi 2, 3 doi thong, 4 doi thong, tu quy
+        ) {
+            return this.players[this.currentPlayer].isValidPlay(this.onBoardCards);
+        }
+        // neu la 2, doi 2, 3 doi thong, 4 doi thong, tu quy thi tu tu roi code tiep, nho xet xem player co onTurn hay khong
+        return true;        
+    }
+
+    private nextPlayer(currPlayer: Player): void {
+        if (this.onBoardCards// .isTwo, isPairOfTwo, is 3 doi thong, 4 doi thong, tu quy
+        ) {
+            this.currentPlayer = (this.currentPlayer + 1) % this.players.length;
+            return;
+        }
+        while (this.players[this.currentPlayer].getOnTurn() === false) {
+            this.currentPlayer = (this.currentPlayer + 1) % this.players.length;
+        }
+        if (this.players[this.currentPlayer] === currPlayer) {
+            this.onBoardCards = [];
+        }
+        for (const player of this.players) {
+            player.setOnTurn(true);
+        }
+    }
+
+    public skipTurn(): void {
+        this.players[this.currentPlayer].setOnTurn(false);
+        this.nextPlayer(this.players[this.currentPlayer]);
+    }
+
+    public playCard(): void {
+        const playedCards = this.players[this.currentPlayer].playCard();
+        this.onBoardCards = playedCards;
+        this.nextPlayer(this.players[this.currentPlayer]);
     }
 }
